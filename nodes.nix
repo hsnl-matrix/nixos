0f67269a77e60db824709a2f3d4a63fc4bf9e1b9
f0x <f0x@cthu.lu>
2022-11-24 23:26:48 +0100

    init aubergine

--- nodes.nix
+++ nodes.nix
@@ -1,6 +1,18 @@
 let
 	secrets = (import ./secrets/secrets.nix);
 in {
+	"aubergine" = {config, pkgs, ...}: {
+		imports = [
+      ./common/all.nix
+			nodes/aubergine/configuration.nix
+		];
+
+		deployment = {
+			secrets = secrets.aubergine;
+			substituteOnDestination = true;
+		};
+	};
+
 	"perzik" = {config, pkgs, ...}: {
 		imports = [
       ./common/all.nix
---
+++ nodes/aubergine/info.nix
@@ -0,0 +1,31 @@
+{
+	hostName = "aubergine";
+	hostId = "3ae1eee7";
+
+	ip = {
+		v4 = "185.206.232.40";
+		v6 = "2a00:51c0:12:1201::23";
+	};
+
+	ports = {
+		synapse-nginx = 8008;
+		synapse-metrics = 9000;
+		synapse-main = 15001;
+		synapse-replication = 15002;
+		synapse-sync = 15003;
+		synapse-fed = 15004;
+		synapse-events = 15005;
+		synapse-typing = 15006;
+		synapse-appservice = 15007;
+		synapse-fed_out1 = 15101;
+		synapse-fed_out2 = 15102;
+		synapse-fed_out3 = 15103;
+		synapse-fed_out4 = 15104;
+
+		mastodon-web = 5500;
+		mastodon-streaming = 5501;
+		mastodon-redis = 5502;
+
+		dex = 4500;
+	};
+}
