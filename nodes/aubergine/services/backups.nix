a8057d82a2bf5a4d24ff7a124fc7d6bcdc18b8d8
f0x <f0x@cthu.lu>
2025-01-24 18:13:28 +0100

    move backups from copia to duplo

--- nodes/aubergine/services/backups.nix
+++ nodes/aubergine/services/backups.nix
@@ -1,24 +1,37 @@
-{
-	self,
-	common,
-	ports,
-	...
+{ self
+, common
+, ports
+, ...
 }:
-{config, pkgs, lib, ...}:
+{ config, pkgs, lib, ... }:
 
 {
-	services.borgbackup.jobs.copia = {
-		paths = "/backup";
-		readWritePaths = ["/backup"];
-		preHook = "${import ../../../common/utility.nix}/bin/backups.js aubergine";
-		repo = "hsnl-aubergine-backup@copia.pixie.town:.";
-		encryption = {
-			mode = "repokey-blake2";
-			passCommand = "cat /persist/secrets/borg";
-		};
-		doInit = true;
-		compression = "zstd";
-		startAt = "*-*-* 4:00:00";
-		extraCreateArgs = "--stats --list --info --exclude-if-present .nobackup --keep-exclude-tags --exclude backup-status";
-	};
-}
+  services.borgbackup.jobs.duplo = {
+    paths = "/backup";
+    preHook = "${import ../../../common/utility.nix}/bin/backups.js aubergine";
+    repo = "backup-hsnl-aubergine@duplo:.";
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /persist/secrets/borg";
+    };
+    prune.keep = {
+      within = "2d";
+      daily = 7;
+      weekly = 3;
+      monthly = 2;
+      yearly = 2;
+    };
+    doInit = true;
+    compression = "zstd";
+    startAt = "*-*-* 4:00:00";
+    extraCreateArgs = "--stats --list --filter=AMEC --info --exclude-if-present .nobackup --keep-exclude-tags --exclude backup-status";
+    environment = {
+      BORG_BASE_DIR = "/persist/root";
+      BORG_RSH = toString
+        (pkgs.writeScript "ssh-opts" ''
+          #!${pkgs.bash}/bin/bash
+          ${pkgs.openssh}/bin/ssh -o ServerAliveInterval=10 -o ServerAliveCountMax=30 "$@"
+        '')
+        };
+    };
+  }
